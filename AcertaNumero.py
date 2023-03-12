e2f736838c48d77ad8820036c46e3ba9e1ae4293
KauaPrado <kauaprado@live.com>
2023-03-12 16:41:11 -0300

    jogo de acertar numero

---
+++ AcertaNumero.py
@@ -0,0 +1,24 @@
+import random as rd
+tentativas =3
+aleatorio =rd.randint(1, 50)
+print(aleatorio)
+print("\nBem vindo ao jogo de adivinhação\n")
+print(aleatorio)
+
+
+while tentativas>0:
+    print("tentativas: ", tentativas)
+    chute = input("Digite um número de 1 até 50 ")
+    if chute.isnumeric():
+        chute = int(chute)
+    
+        if chute ==aleatorio:
+            print("parabéns, você acertou!")
+            break
+        elif chute >aleatorio:
+            print("é maior que o numero secreto " )
+        elif chute<aleatorio:
+            print("é menor que o número secreto" )
+        tentativas -=1
+    else:
+        print("por favor, digite um numero")
